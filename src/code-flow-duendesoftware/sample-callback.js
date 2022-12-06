04fc149475735c6bba88c10264d861bda3c9f313
Hersh Patel <hersh.patel@trinsic.id>
2022-12-06 16:33:25 -0500

    Adding files

--- src/code-flow-duendesoftware/sample-callback.js
+++
@@ -1,9 +0,0 @@
-import { UserManager, settings } from "./sample-settings";
-import { log } from "./sample";
-
-new UserManager(settings).signinCallback().then(function(user) {
-    log("signin response success", user);
-}).catch(function(err) {
-    console.error(err);
-    log(err);
-});
