2403c1d1b3133e5452b15eb52150164214a34051
Hersh Patel <hersh.patel@trinsic.id>
2022-12-07 13:31:05 -0500

    Using modal iframe

--- src/sample.js
+++ src/sample.js
@@ -1,6 +1,3 @@
-// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
 const Log = oidc.Log;
 console.log("Hey3");
 const UserManager = oidc.UserManager;
